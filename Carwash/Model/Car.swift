0628096c0c48683cb98b1255a372fc8fb7fbe08c
Matvey <mnf89@mail.ru>
2020-01-26 19:00:14 +0300

    commit test

---
+++ Carwash/Model/Car.swift
@@ -0,0 +1,13 @@
+//
+//  Car.swift
+//  Carwash
+//
+//  Created by matt_spb on 26/01/2020.
+//  Copyright © 2020 matt_spb_dev. All rights reserved.
+//
+
+import Foundation
+
+class Car {
+    var brand: String?
+}
